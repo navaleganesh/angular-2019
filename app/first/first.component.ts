b80e9ab3c09015b23f12721c5ee2283e7fffc73b
ganesh <ganesh@birdvision.in>
2019-08-02 13:42:55 +0530

    test purpose

---
+++ app/first/first.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+//import { EventEmitter } from 'protractor';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+
+
+}
